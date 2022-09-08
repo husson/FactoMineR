3c08562e6c8b61a698147cd3541332ef8d98585a
Husson <husson@agrocampus-ouest.fr>
2022-09-08 16:35:27 +0200

    disjoncti.h

--- src/disjonctif.h
+++ src/disjonctif.h
@@ -1,7 +1,7 @@
 #ifndef DISJONCTIF_H_
-#define DISJONCTIF_H
+#define DISJONCTIF_H_
 SEXP  disjoVar(SEXP rfact, SEXP rparamint) ;
 SEXP  disjoVarProp(SEXP rfact, SEXP rparamint, SEXP rroww) ;
 SEXP  disjoMat(SEXP rfact, SEXP rparamint, SEXP rniveaux) ;
 SEXP  disjoMatProp(SEXP rfact, SEXP rparamint, SEXP rniveaux, SEXP rroww) ;
-#endif // DISJONCTIF_H
+#endif // DISJONCTIF_H_
